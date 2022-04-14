532741deac20131d45cd3618f7663b441fc25a81
Viennarz Curtiz <viennarzc@gmail.com>
2022-04-14 17:03:37 +0800

    Create Project

---
+++ KidapawanFoodie/KidapawanFoodie/KidapawanFoodieApp.swift
@@ -0,0 +1,20 @@
+//
+//  KidapawanFoodieApp.swift
+//  KidapawanFoodie
+//
+//  Created by Viennarz Curtiz on 4/14/22.
+//
+
+import SwiftUI
+
+@main
+struct KidapawanFoodieApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
